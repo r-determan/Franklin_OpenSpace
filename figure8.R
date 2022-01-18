bcc6121ff4bd8756c4dccd5606629405fe6688e4
Rose Determan <rosed@bu.edu>
2022-01-17 19:49:53 -0500

    update figures and analysis file

--- figure8.R
+++ figure8.R
@@ -1,5 +1,5 @@
 pacman::p_load(dplyr, tidyr, ggplot2, stringr)
-
+getwd()
 # import and summarize data
 r <- read.csv("results2.csv") %>% select(-c(X, per_ch61, budget)) 
 r$type <- factor(r$type, levels = c("Opt.", "Largest", "Least"),
@@ -15,8 +15,9 @@ summary <- r %>%
 #plot
 ggplot()+
   geom_errorbar(data = summary, mapping = aes(x = type, ymin = mean, ymax = mean, color = type),size = 1.5)+
-  geom_jitter(data = r, mapping = aes(x = type, y = value), width = 0.25, alpha = 0.5 )+
+  geom_jitter(data = r, mapping = aes(x = type, y = value), width = 0.25, alpha = 0.75 )+
   ylim(0, NA)+
+  geom_hline(yintercept = 0, size = 0.5, color = "#666666")+
   facet_wrap(facets = vars(name), nrow = 2, scales = "free_y")+
   facet_wrap(~name, 
            nrow = 2, 
@@ -28,10 +29,12 @@ ggplot()+
                                     per_bound_prot = "% Area Borders Protected Land",
                                     efficiency = "Efficiency (%)",
                                     diff_budget = "Difference from Budget ($)"))) + 
-  xlab("")+ylab("")+ labs(title = "Mean Summaries of Parcel Selection Methods")+
+  xlab("")+ylab("")+ labs(title = "Mean Summaries of Parcel Selection Methods",
+                          color = "Selection Method")+
   theme_minimal()+  theme(strip.background = element_blank(),
                           strip.placement = "outside",
-                          legend.position = "none")+
+                          legend.position = "bottom",
+                          axis.text.x = element_text(angle=-70,hjust = 0, vjust = 1))+
   theme(text=element_text(size=20), #change font size of all text
         axis.text=element_text(size=15), #change font size of axis text
         axis.title=element_text(size=15), #change font size of axis titles
@@ -39,7 +42,7 @@ ggplot()+
         legend.text=element_text(size=15), #change font size of legend text
         legend.title=element_text(size=15)) #change font size of legend title  
 
-ggsave("figures/summary.png", width = 12, height = 12/1.618)
+ggsave("figures/summary.png", width = 15, height = 15/1.618)
 
 #-------------------------------------------------------------------------------#
 r$lc <- str_extract(r$scenario, "([A-Z])\\w+$")
@@ -59,7 +62,8 @@ summary <- r %>%
 ggplot()+
   geom_errorbar(data = summary, mapping = aes(x = lc, ymin = mean, ymax = mean, color = lc),size = 1.5)+
   scale_color_manual(values = c("#F37748", "#73956F", "#336699", "#52154E"))+
-  geom_jitter(data = r, mapping = aes(x = lc, y = value), width = 0.25, alpha = 0.5 )+
+  geom_jitter(data = r, mapping = aes(x = lc, y = value), width = 0.25, alpha = 0.75 )+
+  geom_hline(yintercept = 0, size = 0.5, color = "#666666")+
   ylim(0, NA)+
   facet_wrap(facets = vars(name), nrow = 2, scales = "free_y")+
   facet_wrap(~name, 
@@ -72,11 +76,12 @@ ggplot()+
                                       per_bound_prot = "% Area Borders Protected Land",
                                       efficiency = "Efficiency (%)",
                                       diff_budget = "Difference from Budget ($)"))) + 
-  xlab("")+ylab("")+ labs(title = "Mean Summaries of Land Covers")+
+  xlab("")+ylab("")+ labs(title = "Mean Summaries of Land Covers",
+                          color = "Land Cover")+
   theme_minimal()+  theme(strip.background = element_blank(),
                           strip.placement = "outside",
-                          legend.position = "none",
-                          axis.text.x = element_text(angle=-45,hjust = 0, vjust = 1))+
+                          legend.position = "bottom",
+                          axis.text.x = element_text(angle=-70,hjust = 0, vjust = 1))+
   theme(text=element_text(size=20), #change font size of all text
         axis.text=element_text(size=15), #change font size of axis text
         axis.title=element_text(size=15), #change font size of axis titles
@@ -84,5 +89,5 @@ ggplot()+
         legend.text=element_text(size=15), #change font size of legend text
         legend.title=element_text(size=15)) #change font size of legend title  
 
-ggsave("figures/summary_lc.png", width = 12, height = 12/1.618)
+ggsave("figures/summary_lc.png", width = 15, height = 15/1.618)
 
